38dea71c8b5e91422ea582e028aaee379d7f571e
Cody Bonsma <codybonsma@gmail.com>
2020-11-03 22:37:01 -0500

    setup links for the nav compnent to route to pages

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import "../App.css";
+import {Link} from "react-router-dom";
 
 const Nav = () => {
     return (
         <nav>
             <h3>Welcome to my portfolio</h3>
             <ul className="nav-links">
+                <Link className="navStyle" to="/about">
                 <li>About</li>
+                </Link>
+                <Link className="navStyle" to="/portfolio">
                 <li>Portfolio</li>
+                </Link>
+                <Link className="navStyle" to="/contact">
                 <li>Contact</li>
+                </Link>
             </ul>
         </nav>
     );
